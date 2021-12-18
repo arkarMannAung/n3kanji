e00c766431eb9a2ed9ebc7443809d30813667ecb
arkarMannAung <mr.arkarmannaung@gmail.com>
2021-12-18 11:05:07 +0630

    hello

---
+++ bin/main.dart
@@ -0,0 +1,16 @@
+import 'kanjidata.dart';
+
+void main(List<String> args) {
+  for(int i = 0 ; i< data.length ; i++){
+    int compound = data[i]['compound'].length;
+    int furi = data[i]['compound_furigana'].length;
+    int unicode = data[i]['compound_unicode'].length;
+    int zawgyi = data[i]['compound_zawgyi'].length;
+    int eng = data[i]['compound_english'].length;
+    if(compound==furi && compound == unicode && compound==zawgyi && compound==eng){
+      print('index $i valid');
+    }else{
+      print('index $i invalid');
+    }  
+	}
+}
